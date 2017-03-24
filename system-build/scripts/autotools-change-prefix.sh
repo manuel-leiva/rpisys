fbab5bc818928fe3ff70181c628bb0e8c7e7e58f
mleiva <manuelleivaf@gmail.com>
2017-03-23 20:52:19 -0600

    Substitude the paths in the dependency_libs

--- system-build/scripts/autotools-change-prefix.sh
+++ system-build/scripts/autotools-change-prefix.sh
@@ -4,14 +4,17 @@
 PKGNAME=$1
 # Prefix string to be used in pc and la files
 PREFIX=$2
+# Library directory for the normal user-programs installation
+LIBS_INSTALLDIR=$3
 
 # Change library la files
 AUTOTOOLS_LA_LIBS=$(find ${PKGNAME} -name *.la)
-echo AUTOTOOLS_LA_LIBS-num ${#AUTOTOOLS_LA_LIBS[@]}
 if [ -n "${AUTOTOOLS_LA_LIBS}" ]; then
     for i in ${AUTOTOOLS_LA_LIBS}; do
         echo Lib $i ;
-        sed -i "s:libdir=':libdir='${PREFIX}:g" $i ;
+        sed -i "s:^libdir=':libdir='${PREFIX}:g" $i ;
+        # This command is to substitude the paths in the dependency_libs variable
+        sed -i "s: ${LIBS_INSTALLDIR}: ${PREFIX}${LIBS_INSTALLDIR}:g" $i ;
     done ;
 fi
 
@@ -19,7 +22,9 @@ AUTOTOOLS_LAI_LIBS=$(find ${PKGNAME} -name *.lai)
 if [ -n "${AUTOTOOLS_LAI_LIBS}" ]; then
     for i in ${AUTOTOOLS_LAI_LIBS}; do
         echo Lib $i ;
-        sed -i "s:libdir=':libdir='${PREFIX}:g" $i ;
+        sed -i "s:^libdir=':libdir='${PREFIX}:g" $i ;
+        # This command is to substitude the paths in the dependency_libs variable
+        sed -i "s: ${LIBS_INSTALLDIR}: ${PREFIX}${LIBS_INSTALLDIR}:g" $i ;
     done ;
 fi
 
