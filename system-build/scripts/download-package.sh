4f2e683bfac6da47be6133116dfffe0bb4735803
mleiva <manuelleivaf@gmail.com>
2017-04-02 03:46:05 -0600

    Update download target

---
+++ system-build/scripts/download-package.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Package name
+PKG_NAME=$1
+
+PKG_TARGET_NAME=$2
+
+DOWNLOAD_PATH=$3
+
+DOWNLOAD_URL=$4
+
+DESTINATION_PATH=$5
+
+ERRORCOLOR="\033[1;31m"
+INFOCOLOR="\033[0;36m"
+MSGCOLOR="\033[1;33m"
+ENDCOLOR="\033[0m"
+
+# Download tarball package
+function DownloadTar
+{
+    # Verify if the package was already downloaded
+    if [ ! -f ${DOWNLOAD_PATH}/${PKG_TARGET_NAME} ]; then
+        echo -e "${INFOCOLOR}  Download {PKG_NAME}${ENDCOLOR} "
+        wget ${DOWNLOAD_URL}/${PKG_TARGET_NAME} -P ${DOWNLOAD_URL}
+    fi ;
+
+    # Descompres Package
+    # Destination directory
+    mkdir ${DESTINATION_PATH}
+    # Check file owner
+    OWNER=$(ls ${DOWNLOAD_PATH}/${PKG_TARGET_NAME} -l | awk '{print $3 }')
+    if [ "root" == ${OWNER} ]; then
+        echo -e "${MSGCOLOR}You need to be logged in as root you to descompress ${PKG_TARGET_NAME}${ENDCOLOR}"
+        sudo tar -xf ${DOWNLOAD_PATH}/${PKG_TARGET_NAME} -C ${DESTINATION_PATH}
+    else
+        tar -xf ${DOWNLOAD_PATH}/${PKG_TARGET_NAME} -C ${DESTINATION_PATH}
+    fi
+}
+
+# Verify the type of target
+
+case ${PKG_TARGET_NAME} in
+    *.tar.gz)
+        DownloadTar
+        ;;
+
+    *.git)
+        # TODO
+        echo -e "${ERRORCOLOR}ERROR: Target ${PKG_TARGET_NAME} is not supported${ENDCOLOR}"
+        ;;
+    *)
+
+        echo -e "${ERRORCOLOR}ERROR: Target ${PKG_TARGET_NAME} is not supported${ENDCOLOR}"
+        exit 1
+esac
+
+
+
+
+
